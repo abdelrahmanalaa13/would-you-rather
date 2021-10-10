215260671a0bdbaed88afc5f95f50917f58d0069
Abdelrahman Alaa <abdelrahman.alaa@nagwa.com>
2021-10-10 23:02:38 +0200

    Initial Data

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,7 +2,7 @@ import { getInitialData } from "../utils/api";
 import { receiveQuestions } from "./questions";
 import { receiveUsers } from "./users";
 
-export const handelInitialData = () => (dispatch) =>
+export const handleInitialData = () => (dispatch) =>
   getInitialData().then((users, questions) => {
     dispatch(receiveUsers(users));
     dispatch(receiveQuestions(questions));
