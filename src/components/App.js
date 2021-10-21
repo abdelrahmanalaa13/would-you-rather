7c9b0a22fee60d4d07fb7d05934f7694c808bf8a
Abdelrahman Alaa <abdelrahman.alaa@nagwa.com>
2021-10-22 00:38:58 +0200

    Applying navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
-
+import Nav from "./Navbar";
+import LoadingBar from "react-redux-loading";
+import ResultQuestion from "./ResultQuestion";
+import AnsQuestion from "./AnsQuestion";
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
@@ -10,9 +14,23 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        {this.props.loading === true ? null : <Dashboard />}
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <div className="container">
+            <Nav />
+            {this.props.loading === true ? null : (
+              <div>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/ans-question/:id" component={AnsQuestion} />
+                <Route path="/result-question/:id" component={ResultQuestion} />
+                {/* <Route path='/result-question/:id' component={ResultQuestion} /> */}
+                <Route path='/new' component={New} />
+              </div>
+            )}
+          </div>
+        </Fragment>
+      </Router>
     );
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authedUser from "../reducers/authedUser";
 import questions from "../reducers/questions";
 import users from "../reducers/users";
+import { loadingBarReducer } from "react-redux-loading";
 
 export default combineReducers({
   authedUser,
   users,
   questions,
+  loadingBar: loadingBarReducer,
 });
