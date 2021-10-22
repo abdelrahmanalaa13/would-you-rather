bc425e8ac0eef2f6744ab5534efafe3463f9228c
Abdelrahman Alaa <abdelrahman.alaa@nagwa.com>
2021-10-22 20:57:53 +0200

    remove un-used imports

--- src/components/ResultQuestion.js
+++ src/components/ResultQuestion.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { formatDate } from "../utils/helpers";
 import { Progress } from "semantic-ui-react";
+import { Redirect } from "react-router";
 class ResultQuestion extends Component {
   state = {
     chosenAns: "",
